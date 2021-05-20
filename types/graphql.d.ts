8bd4c09afd638dc0344c40f0dc5033e7055b80cd
Deny Herianto <deny.hrnt@gmail.com>
2021-05-20 09:34:27 +0700

    Add graphql schema & queries

---
+++ types/graphql.d.ts
@@ -0,0 +1,18 @@
+
+declare module '*/Pokemon.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Pokemon: DocumentNode;
+
+  export default defaultDocument;
+}
+    
+
+declare module '*/Pokemons.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const Pokemons: DocumentNode;
+
+  export default defaultDocument;
+}
+    
